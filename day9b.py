6f659c79e022546dd096ab8931476c5c7d4f794b
boneillhawk <boneillhawk@users.noreply.github.com>
2017-12-09 15:53:35 -0500

    Day 9 code and notes

---
+++ day9b.py
@@ -0,0 +1,41 @@
+import itertools
+import copy
+import collections
+import heapq
+import math
+import hashlib
+
+def day9b():
+    f = open('input\\input9.txt', 'r')
+    data = [line.strip() for line in f.readlines()][0]
+    f.close()
+
+    depth = 0
+    index = 0
+    total = 0
+    garbage = 0
+    while index < len(data):
+        ch = data[index]
+        if ch == '{':
+            depth += 1
+            total += depth
+            index += 1
+        elif ch == '}':
+            depth -= 1
+            index += 1
+        elif ch == '<':
+            index += 1
+            while data[index] != '>':
+                if data[index] == '!':
+                    index += 2
+                else:
+                    index += 1
+                    garbage += 1
+            index += 1
+        else:
+            index += 1
+        
+    return total, garbage
+        
+    
+print(day9b())
